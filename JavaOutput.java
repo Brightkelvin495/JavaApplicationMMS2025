6355ad470fd19e322d498cfe92a4b187147df445
BrightKelvin495 <brightkelvin495@gmail.com>
2026-02-19 01:21:03 +0100

    Scan

---
+++ JavaOutput.java
@@ -0,0 +1,19 @@
+public class JavaOutput{
+	
+	public static void main(String[] args) {	
+	System.Out.println("Hello world, this is my first Jaava program");
+	System.Out.println("Learning java is fun")
+	
+	
+	//Using the print method
+
+	System.Out.print("Do you love learning Java?");
+	System.Out.print("Yes i love learning Java")
+
+    //System the print method
+	System.Out.print("My name is Bright Kelvin, I am %d years old %n",25)
+	
+	
+   }	
+
+}
